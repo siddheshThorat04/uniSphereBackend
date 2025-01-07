61a76aa7a92f1ea229f871358339e91869688dcf
siddheshThorat04 <siddheshthorat2004@gmail.com>
2025-01-07 23:47:34 +0530

    db connection

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -37,6 +37,7 @@ const getColleges = async (req, res) => {
     try {
         const colleges = await College.find()
         res.status(200).json({colleges:colleges,message:"Colleges Fetched"})
+        
     } catch (error) {
         res.status(400).json({error:error.message})
     }
